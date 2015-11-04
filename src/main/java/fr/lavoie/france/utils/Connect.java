fee89d0d584495f846e1fdfcd99d6b406389237c
cmampuya <chriscomampuya@yahoo.fr>
2015-11-04 01:13:29 +0100

    modification of the pom.xml and the connect class

--- src/main/java/fr/lavoie/france/utils/Connect.java
+++ src/main/java/fr/lavoie/france/utils/Connect.java
@@ -79,39 +79,6 @@ public class Connect {
 		System.out.println(session.isOpen());
 		System.out.println(session.isOpen());
 		session.getTransaction().commit();
-	    /*try {
-
-	      Connection conn = getConnection();
-	      
-	    //Création d'un objet Statement
-	      Statement state = conn.createStatement();
-	      //L'objet ResultSet contient le résultat de la requête SQL
-	      ResultSet result = state.executeQuery("SELECT * FROM disciples");
-	      //On récupère les MetaData
-	      ResultSetMetaData resultMeta = result.getMetaData();
-	         
-	      System.out.println("\n**********************************");
-	      //On affiche le nom des colonnes
-	      for(int i = 1; i <= resultMeta.getColumnCount(); i++)
-	        System.out.print("\t" + resultMeta.getColumnName(i).toUpperCase() + "\t *");
-	         
-	      System.out.println("\n**********************************");
-	         
-	      while(result.next()){         
-	        for(int i = 1; i <= resultMeta.getColumnCount(); i++)
-	          System.out.print("\t" + result.getObject(i).toString() + "\t |");
-	            
-	        System.out.println("\n---------------------------------");
-
-	      }
-
-	      result.close();
-	      state.close();
-	      
-	      System.out.println("Connexion effective !");         
-	         
-	    } catch (Exception e) {
-	      e.printStackTrace();
-	    }      */
+	    
 	  }
 }
